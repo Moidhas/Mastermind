36a3580be3c6a8a7114c2428bc71d0ebfceaf116
Moidhas <moidk3026@gmail.com>
2023-06-16 19:23:53 -0400

    Class outline done

---
+++ main.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Mastermind
+class Mastermind
+end
+
+# Player
+class Player
+end
+
+# Human
+class Human < Player
+end
+
+# Computer
+class Computer < Player
+end
