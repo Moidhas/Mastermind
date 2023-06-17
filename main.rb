adf12d5805c429cc15dff7c4a2ba5ca1dcf286a6
Moidhas <moidk3026@gmail.com>
2023-06-16 20:58:48 -0400

    Added hints for each round

--- main.rb
+++ main.rb
@@ -2,12 +2,17 @@
 
 # Mastermind
 class Mastermind
-
   def initialize
-    comp = Computer.new
-    player = Human.new
+    @comp = Computer.new
+    @human = Human.new
   end
-
+  
+  def play
+    12.times {
+      human.guess_color
+      return puts 'HUMAN WON' if human.guesses == computer.code
+    }
+  end
 end
 
 # Player
@@ -32,18 +37,18 @@ end
 
 # Human
 class Human < Player
-  def initialize
-    super
-    @guesses = Array.new(6)
-  end
+  attr_accessor :guesses
 
   def guess_color
     loop do
-      puts 'Input a number from (1-6): '
-      guess = input_troubles
-      break guess if guess.between?(1, 6)
+      puts 'Input a 4 digit number from with each digit in between 1 to 6: '
+      self.guesses = input_troubles.to_s.split('').map do |digit|
+        digit = Integer(digit)
+        digit if digit.between?(1, 6)
+      end
+      break if guesses.length == 4 && !guesses.include?(nil)
 
-      puts "This guess is not between 1 and 6, Try again.\n"
+      puts "Remember every digit has to be between 1 to 6 and 4 digits long. Try again.\n"
     end
   end
 end
@@ -52,6 +57,19 @@ end
 class Computer < Player
   def initialize
     super
-    @code = Array.new(6).map { rand(1..6) }
+    @code = Array.new(4).map { rand(1..6) }
+  end
+
+  def location_hint(guess)
+    index = 0
+    @correct_location = @code.filter do |digit|
+      state = (digit == guess[index])
+      index += 1
+      state
+    end
+  end
+
+  def number_hint(guess)
+    @correct_number = @code.filter { |digit| guess.include?(digit) } - @correct_location
   end
 end
